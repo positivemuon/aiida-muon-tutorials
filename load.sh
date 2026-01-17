5104adaa695d7da2a43a94c7976701e89b617093
Pietro <bonfus@gmail.com>
2026-01-17 10:08:41 +0100

    Added handson pdf

--- load.sh
+++ load.sh
@@ -4,13 +4,14 @@
 
 curl -JLO "https://github.com/positivemuon/aiida-muon-tutorials/archive/refs/heads/main.zip"
 
-unzip aiida-muon-tutorials-main.zip
+unzip -o aiida-muon-tutorials-main.zip
 cd aiida-muon-tutorials-main/
 
 curl -JLO "https://github.com/positivemuon/aiida-muon-tutorials/releases/download/v0.0.1/CoF2.aiida"
 curl -JLO "https://github.com/positivemuon/aiida-muon-tutorials/releases/download/v0.0.1/Cu.aiida"
 curl -JLO "https://github.com/positivemuon/aiida-muon-tutorials/releases/download/v0.0.1/Fe.aiida"
 curl -JLO "https://github.com/positivemuon/aiida-muon-tutorials/releases/download/v0.0.1/LaFeAsO.aiida"
+curl -JLO "ttps://github.com/positivemuon/aiida-muon-tutorials/releases/download/v0.0.1/handson.pdf"
 
 # unpack data
 find . -name '*.zip' -exec sh -c 'unzip -o "$1" -d "$(dirname "$1")"' _ {} \;
